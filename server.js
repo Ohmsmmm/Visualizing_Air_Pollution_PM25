4af64fce17dedecd31ea07149560c41830512a11
unknown <kiezaza2001@gmail.com>
2021-05-02 19:16:50 +0700

    add query 4

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sql = require("mssql");
 // config for your database
 const config = {
   user: "sa",
-  password: "Ohmsm55343",
+  password: "1234",
   server: "localhost",
   database: "GIS",
   options: {
@@ -14,17 +14,166 @@ const config = {
   },
 };
 const port = 3000;
+Query4a(); //เลือก Query4a, Query4b, Query4c, Query4d
 
-sql
-  .connect(config)
-  .then(async () => {
-    console.log("DB Connected");
-    app.listen(port, () => console.log(`Starting Server on port ${port}`));
-    var request = await new sql.Request();
-    var command = "SELECT * FROM GIS.dbo.AirPollutionPM25";
-    var result = await request.query(command);
-    console.log(result.recordset[0]);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+function Query4a() {
+  sql
+    .connect(config)
+    .then(async () => {
+      console.log("DB Connected");
+      app.listen(port, () => console.log(`Starting Server on port ${port}`));
+      var request = await new sql.Request();
+      var command = "SELECT[country], [city], [Year], [pm25], [geom] FROM [GIS].[dbo].[WHO_AirQuality_Database_2018$] WHERE Year = '2015' AND pm25 > 50";
+      var result = await request.query(command);
+      console.log(result.recordset);
+
+      const Excel = require('exceljs')
+      let workbook = new Excel.Workbook()
+      let worksheet = workbook.addWorksheet('test')
+      worksheet.columns = [
+        { header: 'country', key: 'country' },
+        { header: 'city', key: 'city' },
+        { header: 'Year', key: 'Year' },
+        { header: 'pm25', key: 'pm25' },
+        { header: 'geom', key: 'geom' }
+      ]
+      for (i = 0; i < result.recordset.length; i++) {
+        const data = [result.recordset[i]]
+        data.forEach((e, index) => {
+          const rowIndex = index + i
+          worksheet.addRow({
+            ...e,
+            amountRemaining: {
+              formula: `=C${rowIndex}-D${rowIndex}`
+            },
+            percentRemaining: {
+              formula: `=E${rowIndex}/C${rowIndex}`
+            }
+          })
+        })
+      }
+      workbook.xlsx.writeFile('test.xlsx')
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+function Query4b() {
+  sql
+    .connect(config)
+    .then(async () => {
+      console.log("DB Connected");
+      app.listen(port, () => console.log(`Starting Server on port ${port}`));
+      var request = await new sql.Request();
+      var command = "SELECT	AVG(pm25) AS pm25AVG,[country] FROM [GIS].[dbo].[WHO_AirQuality_Database_2018$] GROUP BY country ORDER BY pm25AVG DESC";
+      var result = await request.query(command);
+      console.log(result.recordset);
+
+      const Excel = require('exceljs')
+      let workbook = new Excel.Workbook()
+      let worksheet = workbook.addWorksheet('test')
+      worksheet.columns = [
+        { header: 'pm25AVG', key: 'pm25AVG' },
+        { header: 'country', key: 'country' }
+      ]
+      for (i = 0; i < result.recordset.length; i++) {
+        const data = [result.recordset[i]]
+        data.forEach((e, index) => {
+          const rowIndex = index + i
+          worksheet.addRow({
+            ...e,
+            amountRemaining: {
+              formula: `=C${rowIndex}-D${rowIndex}`
+            },
+            percentRemaining: {
+              formula: `=E${rowIndex}/C${rowIndex}`
+            }
+          })
+        })
+      }
+      workbook.xlsx.writeFile('test.xlsx')
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+function Query4c() {
+  sql
+    .connect(config)
+    .then(async () => {
+      console.log("DB Connected");
+      app.listen(port, () => console.log(`Starting Server on port ${port}`));
+      var request = await new sql.Request();
+      var command = "SELECT	AVG(pm25) AS pm25AVG, [Year] FROM [GIS].[dbo].[WHO_AirQuality_Database_2018$] WHERE country = 'France' GROUP BY Year ORDER BY pm25AVG DESC";
+      var result = await request.query(command);
+      console.log(result.recordset);
+
+      const Excel = require('exceljs')
+      let workbook = new Excel.Workbook()
+      let worksheet = workbook.addWorksheet('test')
+      worksheet.columns = [
+        { header: 'pm25AVG', key: 'pm25AVG' },
+        { header: 'Year', key: 'Year' }
+      ]
+      for (i = 0; i < result.recordset.length; i++) {
+        const data = [result.recordset[i]]
+        data.forEach((e, index) => {
+          const rowIndex = index + i
+          worksheet.addRow({
+            ...e,
+            amountRemaining: {
+              formula: `=C${rowIndex}-D${rowIndex}`
+            },
+            percentRemaining: {
+              formula: `=E${rowIndex}/C${rowIndex}`
+            }
+          })
+        })
+      }
+      workbook.xlsx.writeFile('test.xlsx')
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+function Query4d() {
+  sql
+    .connect(config)
+    .then(async () => {
+      console.log("DB Connected");
+      app.listen(port, () => console.log(`Starting Server on port ${port}`));
+      var request = await new sql.Request();
+      var command = "SELECT	SUM(population) AS affectedPopulation FROM [GIS].[dbo].[WHO_AirQuality_Database_2018$] WHERE year = '2015' AND color_pm25 = 'yellow'";
+      var result = await request.query(command);
+      console.log(result.recordset);
+
+      const Excel = require('exceljs')
+      let workbook = new Excel.Workbook()
+      let worksheet = workbook.addWorksheet('test')
+      worksheet.columns = [
+        { header: 'affectedPopulation', key: 'affectedPopulation' }
+      ]
+      for (i = 0; i < result.recordset.length; i++) {
+        const data = [result.recordset[i]]
+        data.forEach((e, index) => {
+          const rowIndex = index + i
+          worksheet.addRow({
+            ...e,
+            amountRemaining: {
+              formula: `=C${rowIndex}-D${rowIndex}`
+            },
+            percentRemaining: {
+              formula: `=E${rowIndex}/C${rowIndex}`
+            }
+          })
+        })
+      }
+      workbook.xlsx.writeFile('test.xlsx')
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
